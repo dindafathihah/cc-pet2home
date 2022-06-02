2d7c32f0ded5ce7f5071319859a09ffb9b266d93
dindafathihah <105589164+dindafathihah@users.noreply.github.com>
2022-06-02 19:31:59 +0700

    adding pos

--- controller.js
+++ controller.js
@@ -3,7 +3,6 @@ var conne= require('./connection')
 var md5 = require('md5')
 const { nanoid } = require('nanoid')
 
-
 exports.index= function(req, res){
     res.status(200).json({
         success: true,
@@ -11,21 +10,42 @@ exports.index= function(req, res){
     })
 }
 
-exports.showUserDataHistory = function(req, res){
-    conne.query('select * from user', function(error, rows, fields){
-        res.status(200).json({
-            success: false,
-            rows
-        })
-    })
-};
 
+//CRUD User
+//sukses
+exports.loginUser =  function (req,res){
+    var email = req.body.email
+    var password = md5(req.body.password)
+    let values = [email,password]
+    conne.query('select * from user where email = ? and password = ?', values,function (error, rows) {
+        if (rows.length == 1){
+            res.status(200).json({
+                status:200,
+                error: true,
+                message: 'Login success',
+                user:({
+                    id: rows[0].id_user,
+                    username: rows[0].username
+                })
+            })
+        }else if(rows.length !== 1){
+            res.status(401).json({
+                status:401,
+                error: false,
+                message: 'Login failed. Wrong email or password!',
+            })
+        }
+    })
+}
+
+//sukses
 exports.profileUserId = function (req, res) {
     let id = req.params.id
 
     conne.query('select * from user where id_user = ?', [id], function (error, rows) {
         res.status(200).json({
-            success: false,
+            status:200,
+            success: true,
             profile: ({
                 data:rows
             })
@@ -34,61 +54,8 @@ exports.profileUserId = function (req, res) {
     });
 };
 
-//delete with params(OK)
-exports.deleteUser = function (req, res) {
-    let id = req.params.id
 
-    conne.query('delete from user where id_user = ?', [id], function (error, rows) {
-        if (!error) {
-            res.status(200).json({
-                error: true, 
-                message: 'Success deletes id = '+id
-                }
-            )
-        } else if (error) {
-            res.status(400).json({ 
-                error: false, 
-                message: 'Fail deletes id'
-                }
-            )
-        }
-    });
-};
-
-//update with params(OK)
-exports.editProfile= function (req, res){
-    let id = req.params.id
-    let username=req.body.username;
-    let password=md5(req.body.password);
-    let email=req.body.email;
-    let birth_date=req.body.birth_date;
-    let birth_place=req.body.birth_place;
-    let phone_number=req.body.phone_number;
-    let updatedAt = new Date()
-    let userinstagram = req.body.userinstagram
-
-
-    conne.query('update user set username = ?, email = ?, birth_date = ?, birth_place = ?,  phone_number=?, updatedat=?, userinstagram=? where id_user = ?',
-    [username, email, password, birth_date, birth_place, updatedAt,  phone_number,userinstagram,id ], 
-        function (error, rows, fields){
-            if (error){
-                res.status(500).json({
-                    success: true,
-                    message: 'Fail updates id'
-                    }
-                )
-            } else if (!error) {     
-                res.status(200).json({
-                    success: false,
-                    message: 'Success updates id = '+id
-                    }
-                )
-            }
-        }
-    )
-}
-
-
+//sukses
 exports.addUser = function(req, res){
     var email = req.body.email
     var id  = nanoid(16)
@@ -97,29 +64,31 @@ exports.addUser = function(req, res){
     var phone_number = req.body.phone_number
     var birth_date=req.body.birth_date
     var birth_place=req.body.birth_place 
-    var role="member"
-    var status="active"
+    var role='member'
+    var status='active'
     var createdAt = new Date();
     var updatedAt = createdAt;
     conne.query('select email from user where email = ?',email, function (error, rows) {
         if (rows.length == 1){
             res.status(400).json({
-                success: true,
+                status: 400,
+                success: false,
                 message: 'Email has been taken. Try with another email'
             })
         } else if(rows.length == 0){
             let values =['user_'+id, email, username, password, phone_number, birth_date, birth_place, role, status,createdAt, updatedAt] 
-            conne.query('insert into user (id_user, email, username, password, phone_number, birth_date, birth_place, role, status,createdAt, updatedAt) values (?,?,?,?,?,?,?,?,?,?,?)', values, function(error, rows, fields){
+            conne.query('insert into user (id_user, email, username, password, phone_number, birth_date, birth_place, role, status,created_at, updated_at) values (?,?,?,?,?,?,?,?,?,?,?)', values, function(error, rows, fields){
                 if(error){
                     res.status(500).json({
-                        success: true,
+                        status:500,
+                        success: false,
                         error
-                        }
+                        }   
                     )
                 }else{
                     res.status(200).json({
-                        success: false,
-                        rows,
+                        status:200,
+                        success: true,
                         message: 'Email registered successfully'
                     })
                 }
@@ -128,27 +97,225 @@ exports.addUser = function(req, res){
     })
 }
 
-exports.loginUser =  function (req,res){
-    var email = req.body.email
-    var password = md5(req.body.password)
-    let values = [email,password]
-    conne.query('select * from user where email = ? and password = ?', values,function (error, rows) {
-        if (rows.length == 1){
+//sukses
+exports.deleteUser = function (req, res) {
+    let id = req.params.id
+
+    conne.query('delete from user where id_user = ?', [id], function (error, rows) {
+        if (!error) {
             res.status(200).json({
+                status:200,
+                success: true, 
+                message: 'Success deletes id = '+id
+                }
+            )
+        } else if (error) {
+            res.status(400).json({ 
+                status:400,
+                success: false, 
+                message: 'Fail deletes id'
+                }
+            )
+        }
+    });
+};
+
+//sukses
+exports.editProfile= function (req, res){
+    let id = req.params.id
+    let username=req.body.username;
+    let password=md5(req.body.password);
+    let email=req.body.email;
+    let birth_date=req.body.birth_date;
+    let birth_place=req.body.birth_place;
+    let phone_number=req.body.phone_number;
+    let updated_at = new Date()
+
+    conne.query('update user set username = ?, password=?,email = ?, birth_date = ?, birth_place = ?,  phone_number=?, updated_at=? where id_user = ?',
+    [username, password, email, birth_date, birth_place,  phone_number,updated_at, id ], 
+        function (error, rows, fields){
+            if (error){
+                res.status(500).json({
+                    status:500,
+                    success: false,
+                    error: error
+                    }
+                )
+            } else if (!error) {     
+                res.status(200).json({
+                    status: 200,
+                    success: true,
+                    message: 'Success updates user with id = '+id
+                    }
+                )
+            }
+        }
+    )
+}
+
+
+//CRUD PET
+//sukses
+exports.postPet = function (req,res){
+    var id  = nanoid(16)
+    var id_user = req.body.id_user
+    var tittle = req.body.tittle
+    var breed = req.body.breed
+    var age = req.body.age
+    var location = req.body.location
+    var description = req.body.description
+    //var kontak = req.body.kontak
+    var insta = req.body.insta
+    var createdAt = new Date()
+    var updatedAt = createdAt
+    let values =['posts-'+id, id_user,tittle,breed,age,location,description,insta,createdAt, updatedAt] 
+    conne.query('insert into posts (id_post, id_user, tittle, breed, age,location, description,  insta, created_at, updated_at) values (?,?,?,?,?,?,?,?,?,?)', values, function(error, rows, fields){
+        if(error){
+            res.status(500).send(
+                {status: 500,
+                success: false, 
+                error
+                }
+            )
+        }else{
+            res.status(200).send({
+                status: 200,
                 success: true,
-                rows,
-                message: 'Login success',
-                user:({
-                    id: rows[0].id_user,
-                    username: rows[0].username
-                })
-            })
-        }else if(rows.length !== 1){
-            res.status(401).send({
-                error: false,
-                message: 'Login failed. Wrong email or password!',
+                message: 'Post sent successfully'
             })
         }
     })
 }
 
+//sukses
+exports.getpet = function(req,res){
+
+    conne.query('select * from posts', function (error, rows) {
+        
+        res.status(200).send(
+            {status:200,
+            succes: true, 
+            profile: ({
+                data:rows
+            })
+            }
+        )
+    });
+}
+
+//sukses
+exports.getPetwithIduser =function(req,res){
+    let id = req.params.id
+    conne.query('select * from posts where id_user = ?', [id],function (error, rows) {
+        res.status(200).send(
+            {status:200,
+            success: true, 
+            post: ({
+                data:rows
+            })
+            }
+        )
+    });
+}
+
+//sukses
+exports.getPetwithIdpost =function(req,res){
+    let id = req.params.id
+    conne.query('select * from posts where id_post = ?', [id],function (error, rows) {
+        res.status(200).send(
+            {status:200,
+            succes: true, 
+            post: ({
+                data:rows
+            })
+            }
+        )
+    });
+}
+
+//sukses 
+exports.deletePost = function (req, res) {
+    let id = req.params.id
+
+    conne.query('delete from posts where id_post = ?', [id], function (error, rows) {
+        if (!error) {
+            
+            res.status(200).send(
+                { status:200,
+                success: true, 
+                message: 'Success deletes post with id= '+id
+                }
+            )
+        } else if (error) {
+            res.status(400).send(
+                {
+                status:400,
+                success: false, 
+                message: 'Fail deletes post with id='+id
+                }
+            )
+        }
+    });
+};
+
+//sukses
+exports.editPost= function (req, res){
+    let id = req.params.id
+    var tittle = req.body.tittle
+    var breed = req.body.breed
+    var age = req.body.age
+    var location = req.body.location
+    var description = req.body.description
+    var insta = req.body.insta
+    var updated_at = new Date()
+    let values =[tittle,breed,age,location,description,insta, updated_at, id] 
+    
+    conne.query('update posts set tittle=?, breed=?, age=?, location=?, description=?, insta=?, updated_at=? where id_user=?', values, function(error, rows, fields){
+        
+            if (error){
+                res.status(500).json({
+                    status:500,
+                    success: false,
+                    message: 'Fail updates id',
+                    error
+                    }
+                )
+            } else if (!error) {     
+                res.status(200).json({
+                    status:200,
+                    success: true,
+                    message: 'Success updates id = '+id
+                    }
+                )
+            }
+        }
+    )
+}
+
+exports.getpostbytittle = function (req,res){
+    let title = req.params.title
+    conne.query(`select * from posts where tittle like '%${title}%'`,function (error, rows) {
+        res.status(200).send(
+            {status:200,
+            succes: true, 
+            post: ({
+                data:rows
+            })
+            }
+        )
+    });    
+}
+
+exports.getuserbyname = function (req, res) {
+    let name = req.params.name
+    conne.query(`select * from user where username like '%${name}%'`, function (error, rows) {
+        res.status(200).json({
+            status:200,
+            success: true,
+            profile: ({
+                data:rows
+            })
+            }
+        )
+    });
+};
--- routes.js
+++ routes.js
@@ -3,24 +3,39 @@
 module.exports=function(app){
     var myjson=require('./controller');
     
+    //test api berjalan/tidak
     app.route('/')
         .get(myjson.index);
 
     //Khusus user
-    app.route('/showData')
-        .get(myjson.showUserDataHistory);
     app.route('/profile/:id')
         .get(myjson.profileUserId);
     app.route('/profile/delete/:id')
         .delete(myjson.deleteUser);
-    app.route('/profile/updateData/:id')
+    app.route('/profile/update/:id')
         .put(myjson.editProfile)
     app.route('/register')
         .post(myjson.addUser)
     app.route('/login')
         .post(myjson.loginUser)
-
-    //Isi App
     
+    //Khusus pet
+    app.route('/postPet')
+        .post(myjson.postPet)
+    app.route('/profile/pet/:id')
+        .get(myjson.getPetwithIduser);
+    app.route('/getallpet')
+        .get(myjson.getpet)
+    app.route('/getPetWith/:id')
+        .get(myjson.getPetwithIdpost)
+    app.route('/post/update/:id')
+        .put(myjson.editPost)
+    app.route('/post/delete/:id')
+        .delete(myjson.deletePost)
 
+    //additional
+    app.route('/search/title/:title')
+        .get(myjson.getpostbytittle);
+    app.route('/search/name/:name')
+        .get(myjson.getuserbyname);
 }
